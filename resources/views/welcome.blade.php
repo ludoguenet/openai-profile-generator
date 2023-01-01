90ab916f27ad570400b7eb9c59cdc022b6c7b3b5
ludoguenet <ludovicguenet@gmx.com>
2023-01-01 19:32:01 +0100

    Initial commit.

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Open AI</title>
+    @vite('resources/css/app.css')
+</head>
+<body
+    class="container mx-auto"
+>
+    @yield('content')
+</body>
+</html>
